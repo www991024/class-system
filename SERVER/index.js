e4779dc5599c28847bf1d66ddb98b3c6362c963d
Hao Tai <www991024@gmail.com>
2024-04-14 20:06:44 +0800

    add index.js

--- SERVER/index.js
+++ SERVER/index.js
@@ -32,6 +32,11 @@ app.use(
   passport.authenticate("jwt", { session: false }),
   courseRoute
 );
-app.listen(8080, () => {
-  console.log("Server running on port 8080.");
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log("Server started on port ${port}");
 });
+
+// app.listen(8080, () => {
+//   console.log("Server running on port 8080.");
+// });
