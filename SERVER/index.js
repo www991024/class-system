e5885bdad0933cf755e01fffb3080dee6565daee
Hao Tai <www991024@gmail.com>
2024-04-14 20:14:45 +0800

    add index.js

--- SERVER/index.js
+++ SERVER/index.js
@@ -34,7 +34,7 @@ app.use(
 );
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log("Server started on port ${port}");
+  console.log(`Server started on port ${port}`);
 });
 
 // app.listen(8080, () => {
